291c987375ed009191defc7ae9e84c8286b9c00c
Jeremy Donson <34130568+jeremy-donson@users.noreply.github.com>
2021-09-17 08:30:32 -0400

    Update asdf-bash-functions.sh

--- scripts/asdf-bash-functions.sh
+++ scripts/asdf-bash-functions.sh
@@ -1,13 +1,63 @@
 #!/bin/bash
 
-# Maximize consistency between osx and linux when using asdf across software development languages.
+:' Max consistency when using asdf in bash across software dev languages: funcIndex + funcTests + funcCode
 
-:' Example...
+We will need to keep sharp eyes on several moving targets:
+- asdf versions
+- asdf available plugins
+- asdf 
+- brew supported versions
+
+- bash || zsh
+- 
+
+Implementation Example... [ setUrl | curlScript | sourceScript | testScript | logScriptRun | rmScript ]
+
 ASDF_BASH_FUNCTIONS_SCRIPT_URL="https://raw.githubusercontent.com/urbanspectra-nyc/polylingo/rails_version_update/scripts/asdf-bash-functions.sh"
-ASDF_BASH_FUNCTIONS_SCRIPT_NAME=$( basename "${ASDF_BASH_FUNCTIONS_SCRIPT_URL}" ); echo $ASDF_BASH_FUNCTIONS_SCRIPT_NAME; rm $_
-function sourceWebScript() { curl -o /dev/null --silent -Iw \'%{http_code}\' $1 | source >> load_test_bash_functions.log } 
-# Copy THREE lines above and add to local script, followed by: $ rm $ASDF_BASH_FUNCTIONS_SCRIPT_NAME
-,
+ASDF_BASH_FUNCTIONS_SCRIPT_NAME=$( basename "${ASDF_BASH_FUNCTIONS_SCRIPT_URL}" )
+function sourceWebScript() { curl -o /dev/null --silent -Iw '%{http_code}' $1 | source } 
+sourceWebScript "${ASDF_BASH_FUNCTIONS_SCRIPT_URL}" >> load_test_bash_functions.log
+rm $ASDF_BASH_FUNCTIONS_SCRIPT_NAME
+# Copy FIVE lines above and add to local script to load and test these bash functions.
+
+FILE STRUCTURE
+- FUNCTION INDEX: [ preserveStates, gettersBatch, setupConfigs, loggers, setters, ops, teardowns ]
+- FUNCTION TESTING: 
+- FUNCTION CODE
+'
+
+# FUNCTION INDEX
+
+# getters
+function xStamps() { } # time + date + unixtime + microtime
+function distroFlavorVersion() { }
+function pkgMgrs() { }
+function canBeSudoer() { } # Test: $ sudo bash
+function canBeSudoerNoPass() { } # Test: $ sudo bash
+function canWrite2Repo() { } # Test: $ 
+function canPullRequest2Repo() { } # Test: $ 
+
+# loggers
+function bashLogger() { } # 
+function preserveConfigs() { } # Like env or brew bundle or pip freeze.
+
+# setters
+function asdfPluginsListAllPluginsUpdateRepo() { }
+function asdfPluginsListAllDevLangVersionsUpdateRepo() { }
+function localDevLangDir(cd ; mkdir -p "~/repos/_${1}/" ; cd $_; }
+function localDevLangProjectDir() { } # 
+
+# ops
+
+function asdfInstallPluginListVersions() { asdf plugin-add $1 $2 ; asdf list-all $1 } # Tests:  [ dev_lang_nonexistent, dev_lang_exists, dev_lang_new_undetected ]
+function asdfLocalRubyRailsSetup
+function asdfLocalElixirPhoenixSetup
+
+
+function gitEtc() { }
+function installedMeta() { } # installedUsing; installedAtVersion; configGet; configSet; testedAtVersion # Test using git 
+
+
 
 bash # Subshell supports errors sans crashing terminal window.  Text: $ exit 0
 
@@ -37,5 +87,11 @@ function localDevLangProjectDir() { } #
 function gitEtc() { }
 function installedMeta() { } # installedUsing; installedAtVersion; configGet; configSet; testedAtVersion # Test using git 
 
+# FUNCTION TESTING
 
+function funcTestAllFunctions()
+function funcTestOneFunct()
 
+# FUNCTION CODE
+
+function 
