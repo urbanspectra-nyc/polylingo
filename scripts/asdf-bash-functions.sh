f4dd64610652d14fedf36bc5d6fa1c2799df60fe
Jeremy Donson <34130568+jeremy-donson@users.noreply.github.com>
2021-09-17 06:58:31 -0400

    Update and rename asdf-bash-functions-script.sh to asdf-bash-functions.sh

---
+++ scripts/asdf-bash-functions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Maximize consistency between osx and linux + pan-dev-langs when using asdf.
+function sourceWebScript() { curl -s $ASDF_BASH_FUNCTIONS_SCRIPT_URL | source asdf-bash-functions.sh }
+# Copy line above and add to local script, followed by: $ 
+
+bash # Subshell supports errors sans crashing terminal window.
+
+function distroFlavorVersion() {
+}
+
+function gitEtc() {
+}
+
+function installedMeta() { # installedUsing; installedAtVersion; configGet; configSet; testedAtVersion # git 
+}
+
+function xStamps() { # time + date + unixtime + microtime
+}
+
